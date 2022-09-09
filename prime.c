89f6058d15cd1c493d06adf39b2f4feea86dcd74
zenith-noble01 <webdev686@gmail.com>
2022-09-09 02:31:38 +0100

    the prime function was added

--- prime.c
+++ prime.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 
-int main()
+// using a function
+
+int Find_Fact(int nm)
 {
-    int i, Number, count = 0;
+    int i, Count = 0;
 
-    printf("\n Please Enter any number to Check  = ");
-    scanf("%d", &Number);
-
-    for (i = 2; i <= Number / 2; i++)
+    for (i = 2; i <= nm / 2; i++)
     {
-        if (Number % i == 0)
+        if (nm % i == 0)
         {
-            count++;
-            break;
+            Count++;
         }
     }
-    if (count == 0 && Number != 1)
+    return Count;
+}
+
+int primeFunction()
+{
+    int nm, count = 0;
+
+    printf("\n Please Enter any to Check  \n");
+    scanf("%d", &nm);
+
+    count = Find_Fact(nm);
+    if (count == 0 && nm != 1)
     {
-        printf("\n %d is a Prime Number", Number);
+        printf("\n %d is a Prime", nm);
     }
     else
     {
-        printf("\n %d is Not prime", Number);
+        printf("\n %d is Not", nm);
     }
-
-    primeWhileLoop();
     return 0;
 }
+
+// using the while loop
 
 int primeWhileLoop()
 {
@@ -52,5 +61,34 @@ int primeWhileLoop()
     {
         printf("\n %d is Not Prime", Num);
     }
+    return 0;
+}
+
+int main()
+{
+    int i, Number, count = 0;
+
+    printf("\n Please Enter any number to Check  = ");
+    scanf("%d", &Number);
+
+    for (i = 2; i <= Number / 2; i++)
+    {
+        if (Number % i == 0)
+        {
+            count++;
+            break;
+        }
+    }
+    if (count == 0 && Number != 1)
+    {
+        printf("\n %d is a Prime Number", Number);
+    }
+    else
+    {
+        printf("\n %d is Not prime", Number);
+    }
+
+    primeWhileLoop();
+    primeFunction();
     return 0;
 }
