3f4c2dc9074aa3d1714081afd7d9c668d1c340d3
zenith-noble01 <webdev686@gmail.com>
2022-09-09 02:22:52 +0100

    added the prime number using the while loop

--- prime.c
+++ prime.c
@@ -23,5 +23,34 @@ int main()
     {
         printf("\n %d is Not prime", Number);
     }
+
+    primeWhileLoop();
+    return 0;
+}
+
+int primeWhileLoop()
+{
+    int i = 2, Num, count = 0;
+
+    printf("\n Please Enter any Value \n");
+    scanf("%d", &Num);
+
+    while (i <= Num / 2)
+    {
+        if (Num % i == 0)
+        {
+            count++;
+            break;
+        }
+        i++;
+    }
+    if (count == 0 && Num != 1)
+    {
+        printf("\n %d is a Prime", Num);
+    }
+    else
+    {
+        printf("\n %d is Not Prime", Num);
+    }
     return 0;
 }
